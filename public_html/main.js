08b2b78bd6263caf9fa79569a5ad9b52dde9fe6c
Michael Holloway <mholloway@wikimedia.org>
2017-02-03 13:20:40 -0500

    Add time zone to alpha build timestamp on android-builds.wmflabs.org
    Also close an HTML tag in index.html.
    
    (N.B. I live-hacked this already before discovering this repo, but this
    will at least memorialize the changes.)
    
    Bug: T108318
    Change-Id: I70e5a3cc4b2c04497ff5b66ff9c7f4fc7ccd3701

--- public_html/main.js
+++ public_html/main.js
@@ -2,6 +2,6 @@ $( function() {
     // Fetch the latest build's data
     $.get('runs/latest/meta.json').done( function( data ) {
         var completed_on = new Date( data.completed_on );
-        $( '#last-build-time' ).text( completed_on.toLocaleString() );
+        $( '#last-build-time' ).text( completed_on.toLocaleString(undefined, { timeZoneName: 'short' }) );
     });
 });
